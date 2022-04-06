95b63c1588510692b603e368bc63611d837e14f6
FerEspinozaP <71610246+FerEspinozaP@users.noreply.github.com>
2022-04-06 13:05:13 -0600

    audioList Branch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import {} from react;
+import AudioList from './components/AudioList';
 import './App.css';
 
 function App() {
+
+  const [List, setList] = useState(false);
+
+  const onBackButtonPress = () => {
+    setList(false);
+  }
+
   return (
-    <div className="App">
+    <div className="App m-16">
+
+      {list && <AudioList onBackButtonPress={onBackButtonPress} />}
+      <button onClick={() => setList(true)}>btn</button> 
     </div>
   );
 }
